9f9ded94fa10800d0b8d9fabf487407dd160ec80
Robel Seyoum <robelseyoum3@gmail.com>
2021-10-31 19:02:39 +0000

    clean up

--- app/src/main/java/com/robelseyoum3/cleancode/framework/presentation/notedetail/NoteDetailFragment.kt
+++ app/src/main/java/com/robelseyoum3/cleancode/framework/presentation/notedetail/NoteDetailFragment.kt
@@ -6,8 +6,6 @@ import com.robelseyoum3.cleancode.R
 import com.robelseyoum3.cleancode.framework.presentation.common.BaseNoteFragment
 
 //const val NOTE_DETAIL_STATE_BUNDLE_KEY = "com.codingwithmitch.cleannotes.notes.framework.presentation.notedetail.state"
-
-
 class NoteDetailFragment : BaseNoteFragment(R.layout.fragment_note_detail) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
